3b96bea92b57db6825a96a62085465112c70ece1
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-01-30 11:00:10 -0500

    Update serverExpress.js

--- serverExpress.js
+++ serverExpress.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4001
 app.listen(PORT, ()=> {
-console.log(`The server is listening on ${PORT}.`)
+console.log(`Server is listening on ${PORT}.`)
 });
 
 
